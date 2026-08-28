b9ed017d351108ab64a6b28c8237310d99175c8b
wiiiimm <email@wiiiimm.codes>
2026-08-28 04:01:33 +0000

    Add Linear env types

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,4 +2,7 @@ interface Env {
 	CURSOR_WEBHOOK_URL?: string;
 	CURSOR_WEBHOOK_KEY?: string;
 	SLACK_SIGNING_SECRET?: string;
+	LINEAR_CURSOR_WEBHOOK_URL?: string;
+	LINEAR_CURSOR_WEBHOOK_KEY?: string;
+	LINEAR_WEBHOOK_SECRET?: string;
 }
