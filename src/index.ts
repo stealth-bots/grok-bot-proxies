86b5c2683243ab84e6eda2cc57e92dfb1441f771
wiiiimm <email@wiiiimm.codes>
2026-08-28 04:01:35 +0000

    Route Cloudflare worker /linear to Linear handler

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { handleRequest } from '../api/index';
+import { handleLinearRequest } from '../api/linear';
 
 export { handleRequest, verifySlackSignature, type SlackEnv } from '../api/index';
+export { handleLinearRequest, verifyLinearSignature, type LinearEnv } from '../api/linear';
+
+function isLinearPath(request: Request): boolean {
+	const { pathname } = new URL(request.url);
+	return pathname === '/linear' || pathname === '/api/webhooks/linear' || pathname === '/api/linear';
+}
 
 export default {
 	async fetch(request: Request, env: Env): Promise<Response> {
+		if (isLinearPath(request)) {
+			return handleLinearRequest(request, env);
+		}
 		return handleRequest(request, env);
 	},
 };
